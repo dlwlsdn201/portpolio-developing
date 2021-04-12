12c3979a7c9c00c7f436df7c1a3a3b1de2fae284
LeeJinWoo <dlwlsdn201@naver.com>
2021-04-13 00:51:11 +0900

    headerHeight 수정

--- jquery/header.js
+++ jquery/header.js
@@ -1,7 +1,7 @@
 
 // header 높이에 따른 1페이지 margin-top 값 조절
 $(document).ready(function() {
-    var headerHeight = $(".header-container").innerHeight();
+    var headerHeight = $(".header-container").outerHeight();
 
-    $(".certification-container").css("margin-top", headerHeight-0.1);
+    $(".certification-container").css("margin-top", headerHeight*0.99);
 });
