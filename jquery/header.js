c7eb90c9c0db1cf80c79c189f20a051ea5cec7fe
LeeJinWoo <dlwlsdn201@naver.com>
2021-04-12 21:45:39 +0900

    first commit

---
+++ jquery/header.js
@@ -0,0 +1,7 @@
+
+// header 높이에 따른 1페이지 margin-top 값 조절
+$(document).ready(function() {
+    var headerHeight = $(".header-container").height();
+
+    $(".certification-container").css("margin-top", headerHeight);
+});
