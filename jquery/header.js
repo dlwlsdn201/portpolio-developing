ea70b0155049884ccd0dc380863ce61fde20021a
LeeJinWoo <dlwlsdn201@naver.com>
2021-04-13 10:20:32 +0900

    certification-container의 margin-top 을 padding-top으로 수정

--- jquery/header.js
+++ jquery/header.js
@@ -1,7 +1,7 @@
 
 // header 높이에 따른 1페이지 margin-top 값 조절
 $(document).ready(function() {
-    var headerHeight = $(".header-container").outerHeight();
+    var headerHeight = $(".header-container").innerHeight();
 
-    $(".certification-container").css("margin-top", headerHeight*0.99);
+    $(".certification-container").css("padding-top", headerHeight*0.99);
 });
