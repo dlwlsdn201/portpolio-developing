d54fde24709a7c83018ebffa5c8ab30ffb7f9275
LeeJinWoo <dlwlsdn201@naver.com>
2021-04-13 15:41:48 +0900

    jquery copy 기능

---
+++ jquery/copy.js
@@ -0,0 +1,33 @@
+jQuery( '#copy-tel' ).click(
+    function(e) {
+        let target = document.querySelector('#tel'); 
+        var text = target.textContent;
+        $('#copyBox').val(text);
+        $('#copyBox').select();
+        try{
+            var success = document.execCommand( 'copy' );
+            if(success)
+            alert( '전화번호가 복사 되었습니다.' );
+        }catch(err){
+            alert('이 브라우저에서는 지원하지 않는 기능입니다.');
+        }
+    }
+);
+
+jQuery( '#copy-mail' ).click(
+    function() {
+        let target = document.querySelector('#mail'); 
+        var text = target.textContent;
+        $('#copyBox').val(text);
+
+        $('#copyBox').select();
+
+        try{
+            var success = document.execCommand( 'copy' );
+            if(success)
+            alert( '이메일 주소가 복사 되었습니다.' );
+        }catch(err){
+            alert('이 브라우저에서는 지원하지 않는 기능입니다.');
+        }
+    }
+);
