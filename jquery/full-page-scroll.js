103f4543bba1b0bd38c8af7c10ac1e50bb66d4f8
jinWoo_Lee <dlwlsdn201@naver.com>
2021-05-13 17:09:50 +0900

    test

--- jquery/full-page-scroll.js
+++ jquery/full-page-scroll.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function () {
     var timeStart = 0;
   
--- jquery/header.js
+++ jquery/header.js
@@ -1,4 +1,3 @@
-
 // header 높이에 따른 1페이지 margin-top 값 조절
 $(document).ready(function() {
     var headerHeight = $(".header-container").outerHeight();
