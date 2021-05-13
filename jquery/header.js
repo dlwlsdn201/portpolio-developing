ca73623098e3c4f4f2e8fc91f697386a8e8d393b
jinWoo_Lee <dlwlsdn201@naver.com>
2021-05-13 17:07:48 +0900

    accepted current changes

--- jquery/header.js
+++ jquery/header.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 // header 높이에 따른 1페이지 margin-top 값 조절
 $(document).ready(function() {
@@ -6,13 +5,4 @@ $(document).ready(function() {
 
     $("section").css("margin-top", headerHeight);
     console.log(headerHeight);
-=======
-
-// header 높이에 따른 1페이지 margin-top 값 조절
-$(document).ready(function() {
-    var headerHeight = $(".header-container").outerHeight();
-
-    $("section").css("margin-top", headerHeight);
-    console.log(headerHeight);
->>>>>>> 4c1bc80c1462ac7c2441fe49011a283734c83742
 }); 
